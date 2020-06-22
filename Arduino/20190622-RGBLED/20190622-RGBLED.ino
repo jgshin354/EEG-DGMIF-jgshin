57d53edb084cf84e58c8ff1698020368a98ba0f3
jgshin354 <jgshin354@gmail.com>
2020-06-22 15:00:53 +0900

    Arduino Commit pending
    Arduino Commit pending

--- Arduino/20190622-RGBLED/20190622-RGBLED.ino
+++ Arduino/20190622-RGBLED/20190622-RGBLED.ino
@@ -3,7 +3,7 @@
 #define BLUEPIN 3
 //#define RST 2 //jgshin Add
 #define STATE 7 //jgshin Add
-#define RUN 12 // jgshin ADD
+#define OPERATE 12 // jgshin ADD
 #define TDTPIN0 9
 #define TDTPIN1 10 //jgshin Add
 #define TDTPIN2 11 //jgshin Add
@@ -21,8 +21,7 @@ void setup() {
   pinMode(TDTPIN2, OUTPUT);
 //  pinMode(RST, INPUT);
   pinMode(STATE, INPUT);
-  pinMode(STATE, INPUT);
-
+  pinMode(OPERATE, INPUT);
   digitalWrite(REDPIN, LOW);
   digitalWrite(GREENPIN, LOW);
   digitalWrite(BLUEPIN, LOW);
@@ -30,15 +29,17 @@ void setup() {
 
 //boolean bRST = false;
 boolean bSTATE = false;
+boolean bOPERATE = false;
 int state_n = 0; // 0: Green, 1: Red, 2: Blue 
+int out_pin = 0;
 
 void loop() {
-
-  if(digitalRead(bSTATE) = HIGH){
-    if(bSTATE = false){
+  
+  if(digitalRead(STATE) == HIGH){
+    if(bSTATE == false){
       bSTATE = true;
       state_n = (state_n + 1) % 3;
-      stateRef()
+      stateRef();
     }
   }
   else{
@@ -46,47 +47,126 @@ void loop() {
   }
 
 
+  if(digitalRead(OPERATE) == HIGH){
+    if(bOPERATE == false){
+      bOPERATE = true;
+      operationCode();
+      endRef();
+    }
+  }
+  else{
+    bOPERATE = false;
+  }
+}
+
+void operationCode()
+{
 }
 
 
 void stateRef()
 {
+  switch(state_n){
+    case 0: out_pin = GREENPIN;  break;
+    case 1: out_pin = REDPIN;    break;
+    case 2: out_pin = BLUEPIN;   break;
+    default:out_pin = GREENPIN;  break;
+  }
+  
+  if(state_n == 0) {     //green blinking
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(out_pin, HIGH);
+      delay(100);
+      digitalWrite(out_pin, LOW);
+      delay(100);
+    }
+  } 
+}
+
+
+void endRef()
+{
+  if(state_n == 0){     //green blinking
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(out_pin, HIGH);
+      delay(500);
+      digitalWrite(out_pin, LOW);
+      delay(500);
+    }
+  }
+}
+
+
+
+/*
+
+void stateRef()
+{  
   if(state_n == 0){     //green blinking
     for (int i = 0; i < 3; i++) {
-      digitalWrite(GREENPIN, HIGH)
-      delay(125)
-      digitalWrite(GREENPIN, LOW)
-      delay(125)
+      digitalWrite(GREENPIN, HIGH);
+      delay(100);
+      digitalWrite(GREENPIN, LOW);
+      delay(100);
     }
   }
   else if(state_n ==1){ //red blinking
     for (int i = 0; i < 4; i++) {
-      digitalWrite(REDPIN, HIGH)
-      delay(125)
-      digitalWrite(REDPIN, LOW)
-      delay(125)
+      digitalWrite(REDPIN, HIGH);
+      delay(100);
+      digitalWrite(REDPIN, LOW);
+      delay(100);
     }
   }
   else if(state_n ==2){ //blue blinking
     for (int i = 0; i < 4; i++) {
-      digitalWrite(BLUEPIN, HIGH)
-      delay(125)
-      digitalWrite(BLUEPIN, LOW)
-      delay(125)
+      digitalWrite(BLUEPIN, HIGH);
+      delay(100);
+      digitalWrite(BLUEPIN, LOW);
+      delay(100);
     }
   }
-  
-  
 }
 
 
+
+void endRef()
+{
+  if(state_n == 0){     //green blinking
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(GREENPIN, HIGH);
+      delay(500);
+      digitalWrite(GREENPIN, LOW);
+      delay(500);
+    }
+  }
+  else if(state_n ==1){ //red blinking
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(REDPIN, HIGH);
+      delay(500);
+      digitalWrite(REDPIN, LOW);
+      delay(500);
+    }
+  }
+  else if(state_n ==2){ //blue blinking
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(BLUEPIN, HIGH);
+      delay(500);
+      digitalWrite(BLUEPIN, LOW);
+      delay(500);
+    }
+  }
+}
+
+
+
+*/
 /*
 int count=0;
 int frequency=40; //Unit: Hz, it should be over 1 Hz.
 int treatment_time=10; //Unit: sec.
 int count_limit=treatment_time*frequency;
 int trigger_time=500; //Unit: msec.
-
 int number_subexp=10; //(Number_subexp) times ON/OFF
 int number_subexp_count=0;
 
--- Arduino/readme.md
+++ Arduino/readme.md
@@ -1,19 +1,14 @@
 # Arduino LED control code for EEG signal acquisition
 
-## Code description
+## 20190622-RGBLED
 
 ### `int state_n `
 
 - 0: Green, 1: Red, 2: Blue
 
-### `TDTPIN2 downto 0`
+### `TDTPIN(2 downto 0)`
 
 - 0h: LED off 
 - 1h: green PW, 2h: green CW 
 - 3h: red PW,   4h: red CW 
 - 5h: blue PW,  6h: blue CW 
-
-
-
-
-
